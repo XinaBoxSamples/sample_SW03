f3c5948b15d6da4913e485f49fc7e388fd2a1c86
Brittany Bull <brittanybull19@gmail.com>
2018-07-23 09:49:25 +0200

    Source added

---
+++ SW03CC03_CS11test/SW03CC03_CS11test.ino
@@ -0,0 +1,102 @@
+/**************************************************************************/
+/*!
+    @file     Adafruit_MPL3115A2.cpp
+    @author   K.Townsend (Adafruit Industries)
+    @license  BSD (see license.txt)
+
+    Example for the MPL3115A2 barometric pressure sensor
+
+    This is a library for the Adafruit MPL3115A2 breakout
+    ----> https://www.adafruit.com/products/1893
+
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
+
+    @section  HISTORY
+
+    v1.0 - First release
+*/
+/**************************************************************************/
+
+#include <xCore.h>
+#include <Adafruit_MPL3115A2.h>
+#include <xOD01.h>
+
+#if defined(ESP8266)
+#define RED   RED
+#define GREEN GREEN
+#define BLUE  BLUE
+#include <ESP8266WiFi.h>
+#define Serial Serial
+
+#elif defined(ARDUINO_SAMD_ZERO)
+#define RED   CC03_RED
+#define GREEN CC03_GREEN
+#define BLUE  CC03_BLUE
+#define Serial SerialUSB
+
+#elif defined(ARDUINO_AVR_PRO)
+#define RED   CC01_RED
+#define GREEN CC01_GREEN
+#define BLUE  CC01_BLUE
+#define Serial Serial
+
+#elif defined(ESP32)
+#define RED   CW02_RED
+#define GREEN CW02_GREEN
+#define BLUE  CW02_BLUE
+#define Serial Serial
+#endif
+
+// Power by connecting Vin to 3-5V, GND to GND
+// Uses I2C - connect SCL to the SCL pin, SDA to SDA pin
+// See the Wire tutorial for pinouts for each Arduino
+// http://arduino.cc/en/reference/wire
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+void setup() {
+
+  #if defined(ESP8266)
+  WiFi.forceSleepBegin();
+  Wire.begin(2, 14);
+  Wire.setClockStretchLimit(15000);
+
+#elif defined(ARDUINO_SAMD_ZERO)
+  Wire.begin();
+
+#elif defined(ARDUINO_AVR_PRO)
+  Wire.begin();
+
+#elif defined(ESP32)
+  Wire.begin();
+
+#endif
+  
+  OLED.begin();
+  Serial.begin(115200);
+  
+
+  OLED.begin();
+  OD01.println("Adafruit_MPL3115A2 test!");
+}
+
+void loop() {
+  if (! baro.begin()) {
+    OD01.println("Couldnt find sensor");
+    return;
+  }
+  
+  float pascals = baro.getPressure();
+  // Our weather page presents pressure in Inches (Hg)
+  // Use http://www.onlineconversion.com/pressure.htm for other units
+  OD01.print(pascals/3377); OD01.println(" Inches (Hg)");
+
+  float altm = baro.getAltitude();
+  OD01.print(altm); OD01.println(" meters");
+
+  float tempC = baro.getTemperature();
+  OD01.print(tempC); OD01.println("*C");
+
+  delay(250);
+}
---
+++ SW03CW01test/SW03CW01test.ino
@@ -0,0 +1,57 @@
+/**************************************************************************/
+/*!
+    @file     Adafruit_MPL3115A2.cpp
+    @author   K.Townsend (Adafruit Industries)
+    @license  BSD (see license.txt)
+
+    Example for the MPL3115A2 barometric pressure sensor
+
+    This is a library for the Adafruit MPL3115A2 breakout
+    ----> https://www.adafruit.com/products/1893
+
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
+
+    @section  HISTORY
+
+    v1.0 - First release
+*/
+/**************************************************************************/
+
+#include <xCore.h>
+#include <Adafruit_MPL3115A2.h>
+#include <xOD01.h>
+
+// Power by connecting Vin to 3-5V, GND to GND
+// Uses I2C - connect SCL to the SCL pin, SDA to SDA pin
+// See the Wire tutorial for pinouts for each Arduino
+// http://arduino.cc/en/reference/wire
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+void setup() {
+  Wire.begin(2, 14);
+  OLED.begin();
+  Serial.begin(115200);
+  OD01.println("Adafruit_MPL3115A2 test!");
+}
+
+void loop() {
+  if (! baro.begin()) {
+    OD01.println("Couldnt find sensor");
+    return;
+  }
+  
+  float pascals = baro.getPressure();
+  // Our weather page presents pressure in Inches (Hg)
+  // Use http://www.onlineconversion.com/pressure.htm for other units
+  OD01.print(pascals/3377); OD01.println(" Inches (Hg)");
+
+  float altm = baro.getAltitude();
+  OD01.print(altm); OD01.println(" meters");
+
+  float tempC = baro.getTemperature();
+  OD01.print(tempC); OD01.println("*C");
+
+  delay(250);
+}
